0634883af6f29708b1f7e9fec493e05417a11dcc
Ekkomain <herbertthebert@gmail.com>
2024-06-19 22:44:10 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBQMm4VuHArZODyLaLrDqluxiTvTh3F7z0",
+  authDomain: "crudforme-8ecb5.firebaseapp.com",
+  databaseURL: "https://crudforme-8ecb5-default-rtdb.firebaseio.com",
+  projectId: "crudforme-8ecb5",
+  storageBucket: "crudforme-8ecb5.appspot.com",
+  messagingSenderId: "23077018560",
+  appId: "1:23077018560:web:ebeaaf0d40e3046dec4625"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
